81113ed94a4d888a239697b6ee4f7fefacfe2910
Farhan Ali <87860980+farhanaliofficial@users.noreply.github.com>
2023-08-10 16:49:52 +0500

    Create example.c

---
+++ example.c
@@ -0,0 +1,17 @@
+#include "include/stdrand.h"
+#include <stdio.h>
+
+int main() {
+    long randomNum = rand_int(10, 20);
+    printf("Random number between 10 and 20: %ld\n", randomNum);
+    
+    char choices[] = "ABCDEF";
+    char randomChar = rand_choice_char(choices);
+    printf("Random character: %c\n", randomChar);
+    
+    int numArray[] = {100, 200, 300, 400, 500};
+    long randomIntArray = rand_choice_int(numArray, sizeof(numArray) / sizeof(int));
+    printf("Random integer from array: %ld\n", randomIntArray);
+    
+    return 0;
+}
